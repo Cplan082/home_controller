3fcf01bfb0359d91fdcf119254c1b0dd15415c71
clive_w2 <clive.plante@gmail.com>
2024-02-04 20:51:14 -0500

    L298nDriver.py and tank_controller.py working

--- tank/SW/tank_controller.py
+++ tank/SW/tank_controller.py
@@ -12,9 +12,9 @@ from mapping import dict_buttonMapping
 
 port = 1
 motor_freq = 50
-dict_pin_in = {'in1': 25,
-               'in2': 8,
-               'in3': 1,
+dict_pin_in = {'in1': 23,
+               'in2': 24,
+               'in3': 6,
                'in4': 26,
                }
 
@@ -24,7 +24,7 @@ pi = pigpio.pi()
 if not pi.connected:
     exit()  # Exit if connection to pigpio daemon fails
     
-obj_L298nDriver = L298nDriver(pi, dict_pin_in, motor_freq)
+obj_L298nDriver = L298nDriver(pi, dict_pin_in, motor_freq, invert_a=False, invert_b=False)
 
 try:
     while True:
@@ -36,7 +36,7 @@ try:
         vector_a = yAxis_tank + xAxis_tank
         vector_b = yAxis_tank - xAxis_tank
         
-        obj_L298nDriver(vector_a, vector_b)
+        obj_L298nDriver.drive_motors(vector_a, vector_b)
         
 
     obj_client.killInstance()
@@ -46,3 +46,4 @@ except KeyboardInterrupt:
 
 finally:
     obj_client.killInstance()
+    obj_L298nDriver.shutdown_motors()
